d52ba7c048c6ceb39431306c99c8c8b2beaf6a13
chnspart <imchn24@gmail.com>
2024-03-04 17:37:40 -0500

    new products added

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 
 import React from 'react';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { Header } from "@/components/hero-section";
 import TeamStats from "@/components/team-stats";
 import CompanyInfo from "@/components/company-info";
@@ -12,15 +12,15 @@ import { IoIosCloseCircle } from "react-icons/io";
 import { Button } from '@/components/ui/button';
 
 export default function IndexPage() {
-  const [showPopup, setShowPopup] = useState(false);
+  // const [showPopup, setShowPopup] = useState(false);
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowPopup(true);
-    }, 2000);
+  // useEffect(() => {
+  //   const timeoutId = setTimeout(() => {
+  //     setShowPopup(true);
+  //   }, 2000);
 
-    return () => clearTimeout(timeoutId);
-  }, []); // Empty dependency array ensures this effect runs only once on mount
+  //   return () => clearTimeout(timeoutId);
+  // }, []); 
 
   return (
     <section className="relative grid items-center gap-6 pb-8">
@@ -29,7 +29,7 @@ export default function IndexPage() {
       <CompanyInfo />
       <SaleBanner />
 
-      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
+      {/* {showPopup && <Popup onClose={() => setShowPopup(false)} />} */}
     </section>
   );
 }
@@ -38,21 +38,21 @@ interface PopupProps {
   onClose: () => void;
 }
 
-const Popup: React.FC<PopupProps> = ({ onClose }) => {
-  return (
-    // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70">
-      <div className="relative z-10 max-w-xs rounded-lg bg-white p-8">
-        <CarouselPlugin />
-        <Button
-          variant={"ghost"}
-          size={"icon"}
-          onClick={onClose}
-          className="absolute right-2 top-2 cursor-pointer text-black"
-        >
-          <IoIosCloseCircle size={20} />
-        </Button>
-      </div>
-    </div>
-  );
-};
+// const Popup: React.FC<PopupProps> = ({ onClose }) => {
+//   return (
+//     // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
+//     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70">
+//       <div className="relative z-10 max-w-xs rounded-lg bg-white p-8">
+//         <CarouselPlugin />
+//         <Button
+//           variant={"ghost"}
+//           size={"icon"}
+//           onClick={onClose}
+//           className="absolute right-2 top-2 cursor-pointer text-black"
+//         >
+//           <IoIosCloseCircle size={20} />
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
